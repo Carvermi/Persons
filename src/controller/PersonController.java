5fce7b2d92ce55cc2dc875c47e974980f69790e0
carvermi <carver.mi@yamibuy.com>
2021-11-04 12:57:46 +0800

    Initial commit

---
+++ src/controller/PersonController.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class PersonController {
+
+    public void eat(){
+        System.out.println("吃了一碗大米饭");
+    }
+    public void sleet(){
+        System.out.println("今天活力四射!");
+    }
+}
